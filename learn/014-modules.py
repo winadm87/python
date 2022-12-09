4917aeab5ef2510243905483416e54e3b3399f6f
winadm87 <win.adm87@gmail.com>
2022-12-09 22:01:43 +0400

    work with user functions 2

---
+++ learn/014-modules.py
@@ -0,0 +1,12 @@
+# ----------------------------------
+# This script is used for learning
+# Created by winadm
+# Version 0.1
+# Examples
+# ...
+# ----------------------------------
+import modules
+
+modules.aaa()
+modules.bbb()
+modules.ccc()
---
+++ learn/modules.py
@@ -0,0 +1,13 @@
+# ----------------------------------
+# This script is used for ...
+# Created by winadm
+# Version 0.1
+# Examples
+# ...
+# ----------------------------------
+def aaa():
+    print("A\tAAA")
+def bbb():
+    print("BB\tBB")
+def ccc():
+    print("CCC\tC")
