f7ec9a4e98525ad2b0c0d0b61a6f40914d122a4d
winadm87 <win.adm87@gmail.com>
2022-12-09 10:37:08 +0400

    work with variables

---
+++ learn/002-variables.py
@@ -0,0 +1,20 @@
+# define two variables and print them
+a = "Hello"
+b = "John"
+age = 25
+print(a + ' ' + b)
+# or combine variables into third one and print it instaed
+combined = a + " " + b
+print(combined)
+# and lets convert age to string
+print(combined + " " + str(age))
+# lets work with register
+s_name = "ivanov"
+S_name = "IVANOV"
+print(s_name + " ---- " + S_name)
+# and a little work with numbers
+n1 = 111
+n2 = 555
+sum = n1 + n2
+print(n1+n2)
+print(str(n1) + "+" + str(n2) + "=" + str(sum))
