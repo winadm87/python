30b0630e704fe83584c7207cbecefc689985908e
winadm87 <win.adm87@gmail.com>
2022-12-11 10:37:06 +0400

    work with pycharm - pygame

---
+++ learn/023-pygame.py
@@ -0,0 +1,22 @@
+# ----------------------------------
+# This script is used for learning python
+# 1. Open Pycharm
+# 2. Go to File - Settings
+# 3. Project you are using -> Interpreter and find +
+# 4. Type pygame and install it
+# 5. ... Profit!
+# Created by winadm
+# Version 0.1
+# Examples
+# ...
+# ----------------------------------
+import pygame
+# uncomment two string below and run
+#import pygame.examples.stars
+#pygame.examples.stars.main()
+
+pygame.init()
+screen = pygame.display.set_mode((300, 300))
+while True:
+    pygame.display.flip()
+# close the window with stop button from pycharm))
