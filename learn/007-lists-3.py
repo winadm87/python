f5f4f8dc01c867b8917588efee8a5bbda1afce9e
winadm87 <win.adm87@gmail.com>
2022-12-15 20:43:13 +0400

    work with lists

---
+++ learn/007-lists-3.py
@@ -0,0 +1,16 @@
+# ----------------------------------
+# This script is used for learning
+# Created by winadm
+# Version 0.1
+# Examples
+# ...
+# ----------------------------------
+# lets create a list of charachters
+char_list = [char for char in "learning"]
+print(char_list)
+
+# lets define a tuple of websites
+websites = ("google.com", "github.com", "habr.com")
+# create a list form tuple
+site_list = [site for site in websites]
+print(site_list)
