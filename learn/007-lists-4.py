9858aac3355855d42b6fa4ae1d8283f61a9b4d60
winadm87 <win.adm87@gmail.com>
2022-12-16 21:09:36 +0400

    work with lists

---
+++ learn/007-lists-4.py
@@ -0,0 +1,13 @@
+# ----------------------------------
+# This script is used for learning
+# Created by winadm
+# Version 0.1
+# Examples
+# ...
+# ----------------------------------
+nested1 = [1, [2, 3, 4], [5, 6, 7], 3.4]
+print(nested1[0])
+print(nested1[1])
+print(nested1[1][0])
+print(nested1[2])
+print(nested1[2][0])
