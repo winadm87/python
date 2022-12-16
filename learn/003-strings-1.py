9659d25f648f09098ee31f056fb1547e8f8833e5
winadm87 <win.adm87@gmail.com>
2022-12-16 21:06:23 +0400

    work with strings

---
+++ learn/003-strings-1.py
@@ -0,0 +1,31 @@
+# ----------------------------------
+# This script is used for learning
+# Created by winadm
+# Version 0.1
+# Examples
+# ...
+# ----------------------------------
+this_is_complex_string = "this_is_Complex_String"
+print(f"{this_is_complex_string.capitalize()}") # start string with capital letter
+print(f"{this_is_complex_string.count('i')}") # count symbols in string
+print(f"{this_is_complex_string.find('i')}") # find index of first charachter in string
+print(f"{this_is_complex_string.index('e')}") # find index of element
+print(f"{this_is_complex_string.isalnum()}") # The isalnum() method returns True if all the characters are alphanumeric, meaning alphabet letter (a-z) and numbers (0-9)
+print(f"{this_is_complex_string.isalpha()}") #The isalpha() method returns True if all the characters are alphabet letters (a-z)
+print(f"{this_is_complex_string.isdecimal()}")
+print(f"{this_is_complex_string.isdigit()}")
+print(f"{this_is_complex_string.isidentifier()}") #A string is considered a valid identifier if it only contains alphanumeric letters (a-z) and (0-9), or underscores (_).
+print(f"{this_is_complex_string.islower()}")
+print(f"{this_is_complex_string.isnumeric()}")
+print(f"{this_is_complex_string.isprintable()}")
+print(f"{this_is_complex_string.isspace()}")
+print(f"{this_is_complex_string.istitle()}")
+print(f"{this_is_complex_string.isupper()}")
+print(f"{this_is_complex_string.lower()}") # to lower all characters
+print(f"{this_is_complex_string.lstrip('t')}") # strip letter from the left
+print(f"{this_is_complex_string.rstrip('g')}") # strip charachter from the right
+print(f"{this_is_complex_string.replace('i','o')}") # replace all i to o
+print(f"{this_is_complex_string.split('s')}") # strip string by charachter s
+print(f"{this_is_complex_string.swapcase()}") # swap capitals and non-capitals
+print(f"{this_is_complex_string.title()}") # make all works start from capital
+print(f"{this_is_complex_string.upper()}")
