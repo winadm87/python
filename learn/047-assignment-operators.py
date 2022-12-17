d502c86640b04e608a99ba2266bdbafd8bc138a7
winadm87 <win.adm87@gmail.com>
2022-12-17 09:31:36 +0400

    assignment operators

---
+++ learn/047-assignment-operators.py
@@ -0,0 +1,23 @@
+# ----------------------------------
+# This script is used for learning
+# Created by winadm
+# Version 0.1
+# Examples
+# ...
+# ----------------------------------
+x = 3
+print(x)
+x += 15 # equal to x = x + 15
+print(x)
+x -= 3 # equal to x = x -3
+print(x)
+x *= 2 # eqial to x = x * 2
+print(x)
+x %= 8 # ostatok ot deleniya, 30\8=24, 30-24=6
+print(x)
+x /= 1.5 # divison to float, answer will be 1.5
+print(x)
+x //= 1.5 # division to int, no decimals
+print (x)
+x **= 3 # 2 in power 3, equals 8
+print(x)
