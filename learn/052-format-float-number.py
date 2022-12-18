f8bd3c1940d8b574dc828bb0c923666f11c0c1f1
winadm87 <win.adm87@gmail.com>
2022-12-18 12:30:58 +0400

    format float number

---
+++ learn/052-format-float-number.py
@@ -0,0 +1,15 @@
+# ----------------------------------
+# This script is used for learning
+# Created by winadm
+# Version 0.1
+# Examples
+# ...
+# ----------------------------------
+# application of String Formatting
+# we use .format method, defining 5 digits before dot and 2 digits after
+first_val= 365.48951
+print("String Formatting : {:5.2f}".format(first_val))
+
+# application of String Interpolation
+second_val= 365.48951
+print("String Interpolation: %5.2f" % second_val)
