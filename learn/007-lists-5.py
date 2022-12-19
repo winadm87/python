3867dabe37dc8d1f4f9eecc9c3e2d83eb8f5e4cb
winadm87 <win.adm87@gmail.com>
2022-12-19 11:06:48 +0400

    work with lists

---
+++ learn/007-lists-5.py
@@ -0,0 +1,17 @@
+# ----------------------------------
+# This script is used for learning
+# Created by winadm
+# Version 0.1
+# Examples
+# ...
+# ----------------------------------
+# Create a list of characters using list comprehension
+char_list = [ char for char in "Python" ]
+print(char_list)
+
+# Define a tuple of websites
+websites = ("google.com", "yahoo.com", "history.com", "quora.com")
+
+# Create a list from tuple using list comprehension
+site_list = [ site for site in websites ]
+print(site_list)
